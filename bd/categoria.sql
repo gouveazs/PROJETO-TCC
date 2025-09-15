041a45fdc170bb021965113600594316e14d1654
gouveazs <joaovitorgouveaanasthacio12@gmail.com>
2025-09-15 14:20:13 -0300

    Categorias

--- bd/categoria.sql
+++ bd/categoria.sql
@@ -1,6 +1,4 @@
 insert into categoria values (1, 'Terror'), (2, 'Suspense'), 
 							 (3, 'Romance'), (4, 'Fantasia'), 
 							 (5, 'Biográfico'), (6, 'Ficção científica'), 
-							 (7, 'Comédia'), (8, 'Drama');
-
-delete from categoria where idcategoria = 1;
+							 (7, 'Infantil'), (8, 'Drama');
